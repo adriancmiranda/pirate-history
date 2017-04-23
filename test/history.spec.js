4d9b934ce15bfd65d2826aca954eef09e1ce0c71
Adrian Miranda <adriancmiranda@gmail.com>
2017-04-23 02:20:14 -0300

    test: add dummy test

--- test/history.spec.js
+++ test/history.spec.js
@@ -1,4 +1,8 @@
+const { expect } = require('chai');
 const pirateHistory = require('../');
 
 describe("correct-history", function() {
+  it('should pass', function() {
+    expect(true).to.be.true;
+  });
 });
