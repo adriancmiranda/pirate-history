92391a57f7a17f37e03df53d0cf085bc336bc73a
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-26 19:34:19 -0300

    refactor: structure

--- bin/index.js
+++ bin/index.js
@@ -35,8 +35,8 @@ module.exports = cfg.setConfig(lib => {
   lib.set('dev.env.NODE_ENV', '"development"');
   lib.set('dev.sourceMap', '#cheap-module-eval-source-map');
   lib.set('dev.assetsPublicPath', '/');
-  lib.set('dev.view.entry', './dev-mode.pug');
-  lib.set('dev.entry.test', './dev-mode.js');
+  lib.set('dev.view.entry', './dev-cycle.pug');
+  lib.set('dev.entry.test', './dev-cycle.js');
   lib.set('dev.server.contentBase', '/');
   lib.set('dev.server.compress', true);
   lib.set('dev.server.stats', 'errors-only');
