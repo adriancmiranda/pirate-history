f981a744644b14fb240d67f9c5faaf32ffa1dc2e
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-05-17 06:00:00 -0300

    fix(parse): escaped char

---
+++ source/common/escape-reg-exp.js
@@ -0,0 +1,10 @@
+/*!
+ * @name escapeRegExp
+ *
+ * @return {String}
+ *
+ * @api private
+`*/
+export default function escapeRegExp(rule) {
+	return String(rule).replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
