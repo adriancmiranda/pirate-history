fae4ee39f1937c8f013781d5b26f88dc711fd4a8
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-05-20 16:56:50 -0300

    chore: just a test

--- source/browser/history/hash.js
+++ source/browser/history/hash.js
@@ -1,1 +1,7 @@
-export default {};
+export function push() {
+}
+
+export function replace(hash) {
+	// history.replaceState(undefined, undefined, '#hash');
+	return window.location.replace(`#${hash}`.replace(/^#!?/, ''));
+}
