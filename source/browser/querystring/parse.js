a4937a7d4d26c2ec80ab8da6f1d4e42fdcdcff90
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-05-20 17:14:58 -0300

    chore: add define-property

--- source/browser/querystring/parse.js
+++ source/browser/querystring/parse.js
@@ -1,11 +1,11 @@
 import escapeRegExp from 'source/common/escape-reg-exp';
 
 export default function parse(query, sep = '&', eq = '=') {
-	const params = {};
+	const searchParams = {};
 	const rule = `([^?${eq}${sep}]+)(${escapeRegExp(eq)}([^${sep}]*))?`;
 	const pattern = new RegExp(rule, 'g');
 	String(query).replace(pattern, ($0, $1, $2, $3) => {
-		params[$1] = $3;
+		searchParams[$1] = $3;
 	});
-	return params;
+	return searchParams;
 }
